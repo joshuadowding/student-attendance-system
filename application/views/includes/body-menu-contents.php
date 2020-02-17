18249edb03194abf393cc3548ab92e2949b36a6d
Joshua Dowding <joshuadowding@outlook.com>
2020-02-17 21:29:48 +0000

    break up common parts of the theme to be included in other pages

---
+++ application/views/includes/body-menu-contents.php
@@ -0,0 +1,28 @@
+<section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+    <div class="container">
+
+        <div class="navbar-header">
+            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+                <span class="icon icon-bar"></span>
+            </button>
+
+            <a href="/student-attendance-system" class="navbar-brand">Student Attendance System</a>
+        </div>
+
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-nav-first">
+                <li><a href="/student-attendance-system" class="smoothScroll">Home</a></li>
+                <li><a href="" class="smoothScroll">About</a></li>
+                <li><a href="" class="smoothScroll">Chef</a></li>
+                <li><a href="" class="smoothScroll">Menu</a></li>
+                <li><a href="" class="smoothScroll">Contact</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <a href="/student-attendance-system/index.php/login" class="section-btn">Login</a>
+            </ul>
+        </div>
+    </div>
+</section>
