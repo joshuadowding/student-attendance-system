2507ff8e8ec5f286af0cca488e030726cd68d1d5
janvichaitanyapatel <58836462+janvichaitanyapatel@users.noreply.github.com>
2020-04-29 23:11:15 +0100

    create a room models file

---
+++ application/models/Room.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Room {
+    public $roomID;
+    public $name;
+    public $location;
+    public $capacity;
+    public $pcs;
+    public $printer;
+    public $type;
+}
+?>
