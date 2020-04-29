430966dd8f2ad38b902048d3e018e158e28acc81
janvichaitanyapatel <58836462+janvichaitanyapatel@users.noreply.github.com>
2020-04-29 23:27:18 +0100

    Create Register.php

---
+++ application/models/Register.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//db->modules.students
+class Register{
+	public $registerID;
+	public $studentID;
+	public $startDate;
+	public $endDate;
+}
+?>
